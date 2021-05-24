2b2f1832c6e2bdde776f78eb7763735a2606193c
LoveParrot-mango <76878537+LoveParrot-mango@users.noreply.github.com>
2021-05-23 21:10:57 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDv_SgDk6PGZflSi7Lt5w46_2OoDlBxGl4",
+    authDomain: "kwitter-project-1ed3f.firebaseapp.com",
+    projectId: "kwitter-project-1ed3f",
+    storageBucket: "kwitter-project-1ed3f.appspot.com",
+    messagingSenderId: "933984423356",
+    appId: "1:933984423356:web:c791a346db908ce1462fb9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
